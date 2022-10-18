467353ee70b017bdefb23c3fc59ad676fc5b9ce5
unknown <slashley127@gmail.com>
2022-10-17 22:05:22 -0500

    Chapter 15 exercises complete

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
@@ -7,16 +8,34 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message = "Name is required")
+    @Size(min=3, max=50, message = "Name must be between 3 and 50 characters")
     private String name;
+    @Size(max=500, message = "Description too long")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message = "Email is required")
+    @Email (message = "Invalid email, please try again")
+    private String contactEmail;
+
+    @NotBlank (message = "Location cannot be left blank")
+    @NotNull
+    private String location;
+
+    @Positive(message = "Number of attendees must be one or more")
+    private Integer numberAttendees;
+
+    public Event(String name, String description, String contactEmail, String location, Integer numberAttendees) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.location = location;
+        this.numberAttendees = numberAttendees;
         this.id = nextId;
         nextId ++;
     }
 
+    public Event() {}
     public String getName() {
         return name;
     }
@@ -35,6 +54,30 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getNumberAttendees() {
+        return numberAttendees;
+    }
+
+    public void setNumberAttendees(Integer numberAttendees) {
+        this.numberAttendees = numberAttendees;
     }
 
     @Override
