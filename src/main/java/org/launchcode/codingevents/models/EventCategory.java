4c6da51f56851d7b47adf5bbf679a606c95304d9
unknown <slashley127@gmail.com>
2022-11-16 16:07:16 -0600

    class 17 reading complete

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,7 +3,10 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -11,6 +14,9 @@ public class EventCategory extends AbstractEntity {
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private List<Event> events = new ArrayList<>();
 
     public EventCategory() {}
 
@@ -27,4 +33,7 @@ public class EventCategory extends AbstractEntity {
         return name;
     }
 
+    public List<Event> getEvents() {
+        return events;
+    }
 }
